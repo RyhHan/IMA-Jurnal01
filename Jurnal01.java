046cebf18b676f9c02be65f73838719fce46063a
amraihan <muhraihan23425@gmail.com>
2023-09-28 13:07:19 +0700

    first commit

---
+++ Jurnal01.java
@@ -0,0 +1,7 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        System.out.println("NAMA    : ANDI MUHAMMAD RAIHAN SYAMSU");
+        System.out.println("NIM     : 607062330113");
+        System.out.println("KELAS   : D3IF-47-01");
+    }
+}
